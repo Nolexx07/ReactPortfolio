b84b203833d012163641f63fdf1ce8c37d3154d0
M.Sathvik <sunnysathvik507@gmail.com>
2025-01-24 14:33:41 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
             initial={{ opacity: 0, x: 100 }}
             transition={{ duration: 1 }}
             className="my-4">{CONTACT.phoneNo}</motion.p>
-            <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJvlHdvnzGCXlLnBRcHFSDqCdZJXbsFjpHSHrWDXhdXhGxGJfFvScvmgcjhVMQDlzVDrmhL" className="border-b">{CONTACT.email}</a>
+            <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSMVJHLpPsPMjWRZgGSnsBNFsPStjnxjmcwKlhWqqszGsmBDKxZNljPRQHJQxltzjWXgHrxD">{CONTACT.email}</a>
         </div>
     </div>
   )
